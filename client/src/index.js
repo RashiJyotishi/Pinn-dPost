9bf190a541ff7f3cdfe545767249ea2f4d184640
RashiJyotishi <jyotishirashi1610@gmail.com>
2024-09-15 22:49:29 +0530

    components added

--- client/src/index.js
+++ client/src/index.js
@@ -4,12 +4,28 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route } from 'react-router-dom';
+import Layout from './Layout';
+import Home from './components/Home';
+import About from './components/About';
+
+// Define your routes with the Layout, Home, About, and App components
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      <Route path="" element={<Home />} />
+      <Route path="about" element={<About />} />
+      <Route path="app" element={<App />} /> {/* Add App component route */}
+    </Route>
+  )
 );
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+);
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
